4e557c41b9bcb40de765ee7964fd87af4139a73c
Tzikas <niko.tzikas@gmail.com>
2019-04-26 14:34:02 -0400

    deployment to heroku requires github

---
+++ routes/index.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+
+router.get('/niko', (req,res,next)=>{
+  res.json({hello: 'from niko'})
+})
+
+
+
+
+module.exports = router;
